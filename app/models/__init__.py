8d517650a1d35022b0df7c2557c668dd27741c85
vallas01 <vallasac@gmail.com>
2022-07-21 17:08:12 -0400

    tables working

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,2 +1,5 @@
 from .db import db
 from .user import User
+from .image import Image
+from .restaurant import Restaurant
+from .review import Review
--- app/models/image.py
+++ app/models/image.py
@@ -6,7 +6,7 @@ class Image(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-    review_id = db.Column(db.Integer, db.ForeignKgey('reviews.id'), nullable=True)
+    review_id = db.Column(db.Integer, db.ForeignKey('reviews.id'), nullable=True)
     restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurants.id'), nullable=True)
     title = db.Column(db.String(40), nullable=False)
     img_url = db.Column(db.Text, nullable=False)
