9ec972e2f9392003c50c9e1aae7f5f769bd44cf4
vallas01 <vallasac@gmail.com>
2022-07-22 18:36:26 -0400

    rreview fetch working

--- app/api/review_routes.py
+++ app/api/review_routes.py
@@ -1,12 +1,50 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, session, request
 from flask_login import login_required
-from app.models import Review
+from app.models import Review, db
+from app.forms.review_form import ReviewForm
+
 
 review_routes = Blueprint('review', __name__)
 
 
-@review_routes.route('/')
+def validation_errors_to_error_messages(validation_errors):
+    """
+    Simple function that turns the WTForms validation errors into a simple list
+    """
+    errorMessages = []
+    for field in validation_errors:
+        for error in validation_errors[field]:
+            errorMessages.append(f'{field} : {error}')
+    return errorMessages
+
+
+
+@review_routes.route('')
 @login_required
 def review_get():
     review = Review.query.all()
     return {'review': [review.to_dict() for review in reviews]}
+
+@review_routes.route('', methods=['POST'])
+def review_post():
+    """
+    Creates a new review
+    """
+    form = ReviewForm()
+    # Get the csrf_token from the request cookie and put it into the
+    # form manually to validate_on_submit can be used
+   
+    form['csrf_token'].data = request.cookies['csrf_token']
+    if form.validate_on_submit():
+        new_review = Review(
+            user_id = 1,
+            restaurant_id = 1,
+            text = form.data['text'],
+            rating = form.data['rating'],
+            )
+        db.session.add(new_review)
+        db.session.commit()
+        
+        return new_review.to_dict()
+    return {'errors': validation_errors_to_error_messages(form.errors)}, 401
+   
--- react-app/src/components/ReviewForm.js
+++ react-app/src/components/ReviewForm.js
@@ -1,28 +1,34 @@
 import React, {useState} from 'react';
+import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+import { createReview } from '../../src/store/review'
 
 
 function ReviewForm() {
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const [errors, setErrors] = useState([]);
   const [text, setText] = useState('');
   const [rating, setRating] = useState('');
     
-  // const handleSubmit = (e) => {
-  //   e.preventDefault();
-  //   setErrors([]);
-  //   const user_id = sessionUser.id
-  //   const newReview = {
-  //       user_id,
-  //       restaurant_id,
-  //       text,
-  //       rating
-  //   };
-  //   dispatch(createReview(newReview))
-  //       .then(()=>history.push(`/account`))
-  //       .catch(async (res) => {
-  //       const data = await res.json();
-  //       if (data && data.errors) setErrors(data.errors);
-  //   });
-  //   reset();
-  // }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setErrors([]);
+    // const user_id = sessionUser.id
+    const newReview = {
+        user_id: 1,
+        restaurant_id: 1,
+        text,
+        rating
+    };
+    dispatch(createReview(newReview))
+        .then(()=>history.push(`/account`))
+        .catch(async (res) => {
+        const data = await res.json();
+        if (data && data.errors) setErrors(data.errors);
+    });
+    // reset();
+  }
 
 
 
@@ -31,11 +37,16 @@ function ReviewForm() {
     <>
       <h1> Enter Review </h1>
 
-      {/* <form onSubmit={handleSubmit} className="login-form"> */}
-      <form  className="login-form">
+      {errors.length>0 && (
+      <ul className='error-container'>
+        {errors.map((error, idx) => <li key={idx}>{error}</li>)}
+      </ul>
+      )}
 
+      <form onSubmit={handleSubmit} className="login-form">
+      
                 <label>
-                Enter your dock information...
+                Enter your review information...
                 </label>
                 <input
                     type="text"
