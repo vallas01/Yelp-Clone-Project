d933e619cf666c4e5511e3d19d099b58da2afd62
vallas01 <vallasac@gmail.com>
2022-07-22 11:08:39 -0400

    routes started

---
+++ app/api/image_routes.py
@@ -0,0 +1,12 @@
+from flask import Blueprint, jsonify
+from flask_login import login_required
+from app.models import Image
+
+image_routes = Blueprint('image', __name__)
+
+
+@image_routes.route('/')
+@login_required
+def image_get():
+    image = Image.query.all()
+    return {'image': [image.to_dict() for image in images]}
