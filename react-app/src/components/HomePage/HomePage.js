9525fce90ce116a7200421c1a3f637842a55fef2
vallas01 <vallasac@gmail.com>
2022-07-26 17:16:07 -0400

    homepage images

--- react-app/src/components/HomePage/HomePage.js
+++ react-app/src/components/HomePage/HomePage.js
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './homepage.css'
 
 
 
 function HomePage() {
+    const [count, setCount] = useState(2)
+
+    // useEffect(() => {
+    //     const timer = setInterval(() => {
+    // })
+      
 
     return (
         <>
             <div className='homepage-container'>
-                <img className='homepageImage' src='./restaurant2.jpg' alt='restaurant'></img>
+                {count ===1 && (
+                    <img className='homepageImage' src='./restaurant1.jpg' alt='restaurant'></img>
+                )}
+                {count ===2 && (
+                    <img className='homepageImage' src='./restaurant2.jpg' alt='restaurant'></img>
+                )}
+                {count ===3 && (
+                    <img className='homepageImage' src='./restaurant3.jpg' alt='restaurant'></img>
+                )}
             </div>
             <div className='homepage-tagline'>
                 Heading Out?  Bring Kelp-Me with you
