763fea9c919a99c7e64cf04a754e9ceccf7ce350
Isaac <isaac.g.diaz10@gmail.com>
2022-07-26 15:30:23 -0500

    fixed issue where stars werent updating after the first click

--- react-app/src/components/reviews/newReviewForm.js
+++ react-app/src/components/reviews/newReviewForm.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import { createReview } from '../../store/review'
@@ -15,81 +15,80 @@ function ReviewForm() {
 
   const user = useSelector(state => state.session.user)
   // const restaurant = useSelector(state => state.restaurant)
-  
-    
+
+
   const handleSubmit = (e) => {
     e.preventDefault();
     setErrors([]);
-    
+
     const newReview = {
-        user_id: user.id,
-        restaurant_id: 1,
-        text,
-        rating
+      user_id: user.id,
+      restaurant_id: 1,
+      text,
+      rating
     };
-    
+
     dispatch(createReview(newReview))
-        .then(()=>history.push(`/review`))
-        .catch(async (res) => {
+      .then(() => history.push(`/review`))
+      .catch(async (res) => {
         const data = await res.json();
         if (data && data.errors) setErrors(data.errors);
-    });
+      });
     // reset();
   }
 
- 
 
 
 
-    
+
+
   return (
     <>
       <h1> Enter Review </h1>
 
-      {errors.length>0 && (
-      <ul className='error-container'>
-        {errors.map((error, idx) => <li key={idx}>{error}</li>)}
-      </ul>
+      {errors.length > 0 && (
+        <ul className='error-container'>
+          {errors.map((error, idx) => <li key={idx}>{error}</li>)}
+        </ul>
       )}
 
 
-      
-
-      <form onSubmit={handleSubmit} className="login-form">
-      
-                <label>
-                Enter your review information...
-                </label>
 
 
-                <div className="star-rating">
-                  <input type="radio" id="5-stars" className='rating' value="5" 
-                    onChange={(e) => setRating(e.target.value)} />
-                  <label htmlFor="5-stars" className="star">&#9733;</label>
-                  <input type="radio" id="4-stars" className='rating' value="4" 
-                    onChange={(e) => setRating(e.target.value)} />
-                  <label htmlFor="4-stars" className="star">&#9733;</label>
-                  <input type="radio" id="3-stars" className='rating' value="3" 
-                    onChange={(e) => setRating(e.target.value)} />
-                  <label htmlFor="3-stars" className="star">&#9733;</label>
-                  <input type="radio" id="2-stars" className='rating' value="2" 
-                    onChange={(e) => setRating(e.target.value)} />
-                  <label htmlFor="2-stars" className="star">&#9733;</label>
-                  <input type="radio" id="1-star" className='rating' value="1" 
-                    onChange={(e) => setRating(e.target.value)} />
-                  <label htmlFor="1-star" className="star">&#9733;</label>
-                </div>    
+      <form onSubmit={handleSubmit} className="review-form">
+
+        <label>
+          Enter your review information...
+        </label>
+
+        <div className="star-rating">
+          <input type="radio" id="5-stars" value="5" name="stars"
+            onChange={(e) => setRating(e.target.value)} />
+          <label htmlFor="5-stars">&#9733;</label>
+          <input type="radio" id="4-stars" value="4" name="stars"
+            onChange={(e) => setRating(e.target.value)} />
+          <label htmlFor="4-stars">&#9733;</label>
+          <input type="radio" id="3-stars" value="3" name="stars"
+            onChange={(e) => setRating(e.target.value)} />
+          <label htmlFor="3-stars">&#9733;</label>
+          <input type="radio" id="2-stars" value="2" name="stars"
+            onChange={(e) => setRating(e.target.value)} />
+          <label htmlFor="2-stars">&#9733;</label>
+          <input type="radio" id="1-star" value="1" name="stars"
+            onChange={(e) => setRating(e.target.value)} />
+          <label htmlFor="1-star">&#9733;</label>
+        </div>
 
 
-                
-                <input
-                    type="text"
-                    placeholder='Enter your review...'
-                    value={text}
-                    onChange={(e) => setText(e.target.value)}
-                    required
-                />
-                
+
+        <input
+          type="text"
+          placeholder='Enter your review...'
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          required
+        />
+
         <button className='hostSubmit' type="submit">Submit</button>
       </form>
       <Footer />
