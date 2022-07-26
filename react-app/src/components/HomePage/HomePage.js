20f298b1bf992f6341a31589cd27a016be4da15e
vallas01 <vallasac@gmail.com>
2022-07-26 19:57:26 -0400

    rotates images

--- react-app/src/components/HomePage/HomePage.js
+++ react-app/src/components/HomePage/HomePage.js
@@ -6,13 +6,17 @@ import './homepage.css'
 function HomePage() {
     const [count, setCount] = useState(3)
 
-    // useEffect(() => {
-    //     const timer = setInterval(() => {
+    useEffect(() => {
 
-    // okay
-    // })
-      
-   
+        const timer = setInterval(() => {
+                setCount((count) => count + 1)
+                if (count===3) {setCount(1)}
+        }, 6000);
+        return () => clearInterval(timer)
+    
+    });
+  
+
     return (
         <>
             <div className='homepage-container'>
@@ -22,7 +26,7 @@ function HomePage() {
                 {count ===2 && (
                     <img className='homepageImage' src='https://res.cloudinary.com/kelp-me/image/upload/v1658875820/restaurant2_xsywsl.jpg' alt='restaurant'></img>
                 )}
-                {count ===3 && (
+                {count >= 3 && (
                     <img className='homepageImage' src='https://res.cloudinary.com/kelp-me/image/upload/v1658875796/restaurant3_w9xnyu.jpg' alt='restaurant'></img>
                 )}
             </div>
