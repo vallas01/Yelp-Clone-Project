1c68fb9a657c219786da42e902764cddeac24c33
Joel Cruz <joel.irvin@cruz-family.ws>
2022-07-26 10:36:36 -0700

    added image seeders

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -2,6 +2,7 @@ from flask.cli import AppGroup
 from .users import seed_users, undo_users
 from .restaurants import seed_restaurants, undo_restaurants
 from .reviews import seed_reviews, undo_reviews
+from .images import seed_images, undo_images
 
 # Creates a seed group to hold our commands
 # So we can type `flask seed --help`
@@ -14,6 +15,8 @@ def seed():
     seed_users()
     # Add other seed functions here
     seed_restaurants()
+    seed_reviews()
+    seed_images()
 
 
 # Creates the `flask seed undo` command
@@ -22,3 +25,5 @@ def undo():
     undo_users()
     # Add other undo functions here
     undo_restaurants()
+    undo_reviews()
+    undo_images()
