f10819032b24027f24d84405f81202177bfb76c9
Joel Cruz <joel.irvin@cruz-family.ws>
2022-07-27 11:12:42 -0700

    user page css

--- react-app/src/components/User.js
+++ react-app/src/components/User.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { useSelector } from "react-redux"
+import './UserPage.css'
 
 function User() {
   const [user, setUser] = useState({});
@@ -35,14 +36,17 @@ function User() {
         <strong>Email</strong> {user.email}
       </li>
     </ul>
+
+      <h2 className='user-detail-header' style={{color:'tomato'}}>User Details</h2>
+
     <div className='user-details'>
-      <h1 style={{color:'tomato'}}>User Details</h1>
-      
-        <h3>Name: {user.username}</h3>
-        <h3>Address: {user.address}</h3>
-        <h3>City: {user.city}</h3>
-        <h3>State: {user.state}</h3>
-        <h3>Zip: {user.zip}</h3>
+      <div className='info'>
+        <h4>Name: {user.username}</h4>
+        <h4>Address: {user.address}</h4>
+        <h4>City: {user.city}</h4>
+        <h4>State: {user.state}</h4>
+        <h4>Zip: {user.zip}</h4>
+      </div>
 
       <div className='user-avatar'>
         <img src={user.avatar}/>
